4e27b4d88e2edb054c92f641f023f4e15b2dcdfd
Joao Pedro Orlando <dna809@hotmail.com>
2024-01-07 10:42:57 -0300

    feature: tela de lançamentos

--- src/screens/Entry/index.tsx
+++ src/screens/Entry/index.tsx
@@ -1,17 +1,78 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { View,Text, StyleSheet } from "react-native";
 import { Icon } from  "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { useTranslation } from "react-i18next";
+import BottomSheet, { BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import { Button, RadioButton } from "react-native-paper";
 //local
-import { Container, Body, Spacer, Row} from "./styles";
+import { Container, Body, Spacer, Row, SheetContainer, Footer} from "./styles";
 import Header from "../../components/Header";
 import Colors from "../../../assets/colors";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export enum EntryType{
+    WITHDRAWAL = "WITHDRAWAL",
+    ENTRY = "ENTRY"
+}
 
 function EntryScreen():JSX.Element{
+//navigation--------------------------------------------------
     const navigation = useNavigation();
+//------------------------------------------------------------
+//translate---------------------------------------------------
     const { t } = useTranslation();
-    
+//------------------------------------------------------------
+//bottom-sheet------------------------------------------------
+    const bottomSheetRef = useRef<BottomSheet>(null);
+    const snapPoints = useMemo(() => ['20%'], []);
+    const handleSheetChanges = useCallback((index: number) => {
+        // console.log('handleSheetChanges', index);
+    }, []);
+    const handleClosingSheet = ()=>{
+        bottomSheetRef.current?.close();
+    }
+    const handleOpeningSheet = ()=>{
+        bottomSheetRef.current?.expand();
+    }
+//------------------------------------------------------------
+//inputs------------------------------------------------------
+    const [number, setNumber] = useState('');
+    const [type,setType] = useState(EntryType.ENTRY);
+    const onChangeNumber = (inputValue)=>{
+        const numericValue = inputValue.replace(/[^0-9]/g, '');
+        const formattedValue = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+            minimumFractionDigits: 2,
+        }).format(parseInt(numericValue) / 100);
+        setNumber(formattedValue);
+    }
+    const onChangeType = (radioValue)=>{
+
+    }
+//------------------------------------------------------------
+//date-picker-------------------------------------------------
+    const [date, setDate] = useState(new Date(new Date().getTime()));
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
+    };
+
+    const showDatepicker = () => {
+        setShow(true);
+    };
+
+    const formatDate = (date):string => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return date.toLocaleDateString(undefined, options);
+    };
+//------------------------------------------------------------
+
     return (
         <Container>
             <Header
@@ -28,9 +89,70 @@ function EntryScreen():JSX.Element{
             />
             <Body>
                 <Spacer/>
+                {show && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        value={date}
+                        is24Hour={true}
+                        onChange={onChange}
+                    />
+                )}
             </Body>
+            <BottomSheet
+                ref={bottomSheetRef}
+                index={0}
+                snapPoints={snapPoints}
+                onChange={handleSheetChanges}
+                enablePanDownToClose={false}
+            >
+                <SheetContainer>
+                    <Row>
+                        <BottomSheetTextInput 
+                            value={number}
+                            placeholder="R$ 0,00"
+                            keyboardType="numeric" 
+                            style={styles.input} 
+                            onChangeText={onChangeNumber}
+                        />
+                        <TouchableOpacity onPress={showDatepicker}>
+                            <BottomSheetTextInput 
+                                value={formatDate(date)}
+                                placeholder=""
+                                editable={false}
+                                style={styles.input} 
+                            />
+                        </TouchableOpacity>
+                    </Row>
+                    <Row>
+                        <RadioButton.Group onValueChange={onChangeType} value={type}>
+                            <View>
+                                <Text>{t('entry')}</Text>
+                                <RadioButton value={EntryType.ENTRY} />
+                            </View>
+                            <View>
+                                <Text>{t('withdrawal')}</Text>
+                                <RadioButton value={EntryType.WITHDRAWAL} />
+                            </View>
+                        </RadioButton.Group>
+                    </Row>
+                </SheetContainer>
+            </BottomSheet>
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+      marginTop: 8,
+      marginBottom: 10,
+      borderRadius: 10,
+      fontSize: 16,
+      lineHeight: 20,
+      padding: 8,
+      backgroundColor: 'rgba(151, 151, 151, 0.25)',
+      minWidth:150,
+      marginHorizontal:10
+    },
+  });
 
 export default EntryScreen;
--- src/screens/Entry/styles.ts
+++ src/screens/Entry/styles.ts
@@ -1,17 +1,23 @@
 import styled from 'styled-components/native';
 import Colors from '../../../assets/colors';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
     flex:1;
+    justify-content: flex-end;
     background-color: ${Colors.primary.blue};
 `;
 
-export const Body = styled.View`
+export const Footer = styled.View`
     flex:1;
+    background-color: beige;
+`;
+
+export const Body = styled.View`
+    flex:3;
     padding: 10px;
     background-color: ${Colors.primary.white};
-    border-top-left-radius: 32px;
-    border-top-right-radius: 32px;
+    /* border-top-left-radius: 32px;
+    border-top-right-radius: 32px; */
 `;
 
 export const Spacer = styled.View`
@@ -22,4 +28,13 @@ export const Row = styled.View`
     display: flex;
     flex-direction: row;
     /* background-color: beige; */
+`;
+
+export const SheetContainer = styled.View`
+    flex: 1;
+    padding: 0px 20px 0px 20px;
+`;
+
+export const RadioText = styled.Text`
+    text-transform: capitalize;
 `;
